857cbab8fb8d84ccc5ff893e751ea85431a74c49
Aryan Sidhwani <71324887+silverstone-git@users.noreply.github.com>
2025-05-29 13:08:26 +0530

    Update mongodb_index_creator.py
    Fixed according to latest pymongo spec

--- TextEmbedder/mongodb_index_creator.py
+++ TextEmbedder/mongodb_index_creator.py
@@ -1,27 +1,40 @@
+from pymongo.operations import SearchIndexModel
+import time
 
-def create_vector_index(mongo_client, collection, index_name, num_dimensions: int):
-    try:
+def create_vector_index(mongo_client, collection, index_name, num_dimensions: int= 3072):
+    search_index_model= SearchIndexModel(definition= {
+            "fields": [
+                {
+                    "type": "vector",
+                    "path": "embedding",
+                    "similarity": "dotProduct",
+                    "numDimensions": num_dimensions,
+                    "quantization": "scalar"
+                }
+            ]
+        },
 
-        index = {
-            "name": f"{index_name}",
-            "type": "vectorSearch",
-            "definition": {
-                "fields": [
-                    {
-                        "type": "vector",
-                        "path": "embedding",
-                        "similarity": "dotProduct",
-                        "numDimensions": num_dimensions
-                    }
-                ]
-            }
-        }
+        name= index_name,
+        type= "vectorSearch"
+    )
 
-        # Create the search index
-        result = collection.create_index(index)
-        print(result)
-    finally:
-        mongo_client.close()
+
+    # Create the search index
+    result = collection.create_search_index(model=search_index_model)
+    print("New search index named " + result + " is building.")
+
+    # Wait for initial sync to complete
+    print("Polling to check if the index is ready. This may take up to a minute.")
+    predicate=None
+    if predicate is None:
+      predicate = lambda index: index.get("queryable") is True
+    while True:
+      indices = list(collection.list_search_indexes(result))
+      if len(indices) and predicate(indices[0]):
+        break
+      time.sleep(5)
+    print(result + " is ready for querying.")
+
 
 if __name__ == "__main__":
     print("mongodb index creation file")
